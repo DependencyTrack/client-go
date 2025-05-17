10d84ba85e5486296b2151fb0ddcfb67387df7ce
SolarFactories <SolarFactories@users.noreply.github.com>
2025-05-17 21:29:07 +0100

    Added remainder of project, policy, notificationRule methods to TagService.
    Signed-off-by: SolarFactories <SolarFactories@users.noreply.github.com>

--- tag.go
+++ tag.go
@@ -23,6 +23,17 @@ type TagListResponseItem struct {
 	NotificationRuleCount int64  `json:"notificationRuleCount,omitempty"`
 }
 
+type TaggedProjectListResponseItem struct {
+	UUID    uuid.UUID `json:"uuid,omitempty"`
+	Name    string    `json:"name,omitempty"`
+	Version string    `json:"version,omitempty"`
+}
+
+type TaggedPolicyListResponseItem struct {
+	UUID uuid.UUID `json:"uuid,omitempty"`
+	Name string    `json:"name,omitempty"`
+}
+
 func (ts TagService) Create(ctx context.Context, names []string) (err error) {
 	req, err := ts.client.newRequest(ctx, http.MethodPut, "/api/v1/tag", withBody(names))
 	if err != nil {
@@ -59,10 +70,100 @@ func (ts TagService) Delete(ctx context.Context, names []string) (err error) {
 }
 
 func (ts TagService) TagProjects(ctx context.Context, tag string, projects []uuid.UUID) (err error) {
-	req, err := ts.client.newRequest(ctx, http.MethodPost, fmt.Sprintf("/api/v1/%s/project", tag), withBody(Map(projects, uuid.UUID.String)))
+	req, err := ts.client.newRequest(ctx, http.MethodPost, fmt.Sprintf("/api/v1/tag/%s/project", tag), withBody(projects))
 	if err != nil {
 		return
 	}
 	_, err = ts.client.doRequest(req, nil)
+	return
+}
+
+func (ts TagService) UntagProjects(ctx context.Context, tag string, projects []uuid.UUID) (err error) {
+	req, err := ts.client.newRequest(ctx, http.MethodDelete, fmt.Sprintf("/api/v1/tag/%s/project", tag), withBody(projects))
+	if err != nil {
+		return
+	}
+	_, err = ts.client.doRequest(req, nil)
+	return
+}
+
+func (ts TagService) GetProjects(ctx context.Context, tag string, po PageOptions, so SortOptions) (p Page[TaggedProjectListResponseItem], err error) {
+	req, err := ts.client.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/tag/%s/project", tag), withPageOptions(po), withSortOptions(so))
+	if err != nil {
+		return
+	}
+
+	res, err := ts.client.doRequest(req, &p.Items)
+	if err != nil {
+		return
+	}
+
+	p.TotalCount = res.TotalCount
+	return
+}
+
+func (ts TagService) TagPolicies(ctx context.Context, tag string, policies []uuid.UUID) (err error) {
+	req, err := ts.client.newRequest(ctx, http.MethodPost, fmt.Sprintf("/api/v1/tag/%s/policy", tag), withBody(policies))
+	if err != nil {
+		return
+	}
+	_, err = ts.client.doRequest(req, nil)
+	return
+}
+
+func (ts TagService) UntagPolicies(ctx context.Context, tag string, policies []uuid.UUID) (err error) {
+	req, err := ts.client.newRequest(ctx, http.MethodDelete, fmt.Sprintf("/api/v1/tag/%s/policy", tag), withBody(policies))
+	if err != nil {
+		return
+	}
+	_, err = ts.client.doRequest(req, nil)
+	return
+}
+
+func (ts TagService) GetPolicies(ctx context.Context, tag string, po PageOptions, so SortOptions) (p Page[TaggedPolicyListResponseItem], err error) {
+	req, err := ts.client.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/tag/%s/policy", tag), withPageOptions(po), withSortOptions(so))
+	if err != nil {
+		return
+	}
+
+	res, err := ts.client.doRequest(req, &p.Items)
+	if err != nil {
+		return
+	}
+
+	p.TotalCount = res.TotalCount
+	return
+}
+
+func (ts TagService) TagNotificationRules(ctx context.Context, tag string, rules []uuid.UUID) (err error) {
+	req, err := ts.client.newRequest(ctx, http.MethodPost, fmt.Sprintf("/api/v1/tag/%s/notificationRule", tag), withBody(rules))
+	if err != nil {
+		return
+	}
+	_, err = ts.client.doRequest(req, nil)
+	return
+}
+
+func (ts TagService) UntagNotificationRules(ctx context.Context, tag string, rules []uuid.UUID) (err error) {
+	req, err := ts.client.newRequest(ctx, http.MethodDelete, fmt.Sprintf("/api/v1/tag/%s/notificationRule", tag), withBody(rules))
+	if err != nil {
+		return
+	}
+	_, err = ts.client.doRequest(req, nil)
+	return
+}
+
+func (ts TagService) GetNotificationRules(ctx context.Context, tag string, po PageOptions, so SortOptions) (p Page[TaggedPolicyListResponseItem], err error) {
+	req, err := ts.client.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/tag/%s/notificationRule", tag), withPageOptions(po), withSortOptions(so))
+	if err != nil {
+		return
+	}
+
+	res, err := ts.client.doRequest(req, &p.Items)
+	if err != nil {
+		return
+	}
+
+	p.TotalCount = res.TotalCount
 	return
 }
